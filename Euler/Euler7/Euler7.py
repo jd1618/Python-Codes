e8edd80784cae96cc91fd90c9d10b8b8a8339249
Jack Davies <67157618+jd1618@users.noreply.github.com>
2021-01-22 11:49:44 +0000

    Update Euler7.py

--- Euler/Euler7/Euler7.py
+++ Euler/Euler7/Euler7.py
@@ -1,1 +1,27 @@
+import math
+import time
 
+tests = int(input())
+N = [int(input()) for i in range(tests)]
+
+start = time.time()
+
+def func(num):
+    p = 3
+    primes = [2]
+    while len(primes) < num:
+        prime = True
+        for i in range(2, int(math.sqrt(p) + 1)):
+            if p % i == 0: 
+                prime = False
+                break 
+        if prime: 
+            primes.append(p)
+        p += 1
+    return primes[-1]
+
+for num in N:
+    print(func(num))
+
+end = time.time()
+print(end - start)
