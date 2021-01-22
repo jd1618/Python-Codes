7f62e344cc39a7067168e0a347b19cf7e7b9037c
Jack Davies <67157618+jd1618@users.noreply.github.com>
2021-01-22 00:48:29 +0000

    Update Euler1_Simple.py

--- Euler/Euler1/Euler1_Simple.py
+++ Euler/Euler1/Euler1_Simple.py
@@ -5,25 +5,25 @@
 
 import time
 
-tests = int(input('Number of test cases: '))         # <--- set number of test cases
+tests = int(input('Number of test cases: '))                # <--- set number of test cases
 N = [int(input('Test case ' + str(i + 1) + ': ')) \
-     for i in range(tests)]                          # <--- set num for each case
+     for i in range(tests)]                                 # <--- set num for each case
 
-start = time.time()                                  # <--- initiates timer
+start = time.time()                                         # <--- initiates timer
 
 def func(num):
     ''' 
     Sums all multiples of 3 and 5 below num
     '''
     sum = 0
-    for i in range(3, num):                          # <--- 3 is the first number to keep
+    for i in range(3, num):                                 # <--- 3 is the first number to keep
         if i % 3 == 0 or i % 5 == 0:
             sum += i
     return sum
 
 for num in N:
     print('Solution ' + str(N.index(num) + 1) + \
-          ': ' + str(func(num)))                     # <--- prints the corresponding sums
+          ': ' + str(func(num)))                            # <--- prints the corresponding sums
     
-end = time.time()                                    # <--- time after code termination
-print(end - start)                                   # <--- prints execution time
+end = time.time()                                           # <--- time after code termination
+print('Time execution: ' + str(end - start) + ' s')         # <--- prints execution time
