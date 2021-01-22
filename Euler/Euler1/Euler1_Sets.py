e6e0f4601ad029954ae79646552bd6065373adbd
Jack Davies <67157618+jd1618@users.noreply.github.com>
2021-01-22 00:28:08 +0000

    Update Euler1_Sets.py

--- Euler/Euler1/Euler1_Sets.py
+++ Euler/Euler1/Euler1_Sets.py
@@ -1,1 +1,17 @@
+import time
 
+tests = int(input('Number of test cases: '))         
+N = [int(input('Test case ' + str(i + 1) + ': ')) \
+     for i in range(tests)]                          
+
+start = time.time()                                  
+
+def func(num):
+    return sum({i for i in range(3, num, 3)}.union({i for i in range(5, num, 5)}))
+
+for num in N:
+    print('Solution ' + str(N.index(num) + 1) + \
+          ': ' + str(func(num)))  
+                                                     
+end = time.time()                                   
+print(end - start)                                  
