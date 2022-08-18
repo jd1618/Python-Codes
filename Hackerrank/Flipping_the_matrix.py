eed581c1e9c22fdc0635e774475973c3cc994cfa
Jack Davies <67157618+jd1618@users.noreply.github.com>
2022-08-18 18:28:42 +0100

    Delete Hackerrank directory

--- Hackerrank/Flipping_the_matrix.py
+++
@@ -1,5 +0,0 @@
-def flippingMatrix(matrix):
-  '''
-  A function that takes as input a 2n x 2n matrix and looks to reverse rows and columns as 
-  many times as possible to maximise the sum of the elements in the upper left n x n matrix.
-  '''
